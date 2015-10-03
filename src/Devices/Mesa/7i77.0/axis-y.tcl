305dc885e107e25de04ee5ca9cb1319ca6d0e69a
torvn77 <torvn77@mail.ru>
2015-10-03 22:57:38 +0300

    Update and rename axis-y.hal to axis-y.tcl

--- src/Devices/Mesa/7i77.0/axis-y.tcl
+++ src/Devices/Mesa/7i77.0/axis-y.tcl
@@ -12,15 +12,15 @@ net axis.1.pos-lim-sw-in <= y-pos-limit
 
 loadrt pid pid.y
 addf pid.y.do-pid-calcs servo-thread
-setp   pid.y.Pgain     [AXIS_1]P
-setp   pid.y.Igain     [AXIS_1]I
-setp   pid.y.Dgain     [AXIS_1]D
-setp   pid.y.bias      [AXIS_1]BIAS
-setp   pid.y.FF0       [AXIS_1]FF0
-setp   pid.y.FF1       [AXIS_1]FF1
-setp   pid.y.FF2       [AXIS_1]FF2
-setp   pid.y.deadband  [AXIS_1]DEADBAND
-setp   pid.y.maxoutput [AXIS_1]MAX_OUTPUT
+setp   pid.y.Pgain     S::AXIS_1(P)
+setp   pid.y.Igain     S::AXIS_1(I)
+setp   pid.y.Dgain     S::AXIS_1(D)
+setp   pid.y.bias      S::AXIS_1(BIAS)
+setp   pid.y.FF0       S::AXIS_1(FF0)
+setp   pid.y.FF1       S::AXIS_1(FF1)
+setp   pid.y.FF2       S::AXIS_1(FF2)
+setp   pid.y.deadband  S::AXIS_1(DEADBAND)
+setp   pid.y.maxoutput S::AXIS_1(MAX_OUTPUT)
 
 net y-index-enable  <=>  pid.y.index-enable
 net y-enable       => pid.y.enable
@@ -35,9 +35,9 @@ net y-pos-fb       => pid.y.feedback
 
 # ---PWM Generator signals/setup---
 
-setp hm2_5i25.0.7i77.0.1.analogout1-scalemax [AXIS_1]OUTPUT_SCALE
-setp hm2_5i25.0.7i77.0.1.analogout1-minlim   [AXIS_1]OUTPUT_MIN_LIMIT
-setp hm2_5i25.0.7i77.0.1.analogout1-maxlim   [AXIS_1]OUTPUT_MAX_LIMIT
+setp hm2_5i25.0.7i77.0.1.analogout1-scalemax S::AXIS_1(OUTPUT_SCALE)
+setp hm2_5i25.0.7i77.0.1.analogout1-minlim   S::AXIS_1(OUTPUT_MIN_LIMIT)
+setp hm2_5i25.0.7i77.0.1.analogout1-maxlim   S::AXIS_1(OUTPUT_MAX_LIMIT)
 
 net hm2_5i25.0.7i77.0.1.analogout1 <= y-output
 
@@ -48,7 +48,7 @@ setp hm2_5i25.0.encoder.01.filter 1
 setp hm2_5i25.0.encoder.01.index-invert 0
 setp hm2_5i25.0.encoder.01.index-mask 0
 setp hm2_5i25.0.encoder.01.index-mask-invert 0
-setp hm2_5i25.0.encoder.01.scale [AXIS_1]ENCODER_SCALE
+setp hm2_5i25.0.encoder.01.scale S::AXIS_1(ENCODER_SCALE)
 
 net hm2_5i25.0.encoder.01.position      => y-pos-fb
 net hm2_5i25.0.encoder.01.velocity      => y-vel-fb
