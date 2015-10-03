3214325179893a1f0cf6235f89c2a8da08dbad21
torvn77 <torvn77@mail.ru>
2015-10-03 22:35:11 +0300

    Update and rename 7i76.hal to 7i76.tcl

---
+++ src/Devices/Mesa/7i76.0/7i76.tcl
@@ -0,0 +1,8 @@
+source Devices/Mesa/7i76.0/5i25-6i25.tcl
+source Devices/Mesa/7i76.0/gpio.tcl
+source Devices/Mesa/7i76.0/spindle.tcl
+source Devices/Mesa/7i76.0/axis-x.tcl
+source Devices/Mesa/7i76.0/axis-y.tcl
+source Devices/Mesa/7i76.0/axis-z.tcl
+source Devices/Mesa/7i76.0/axis-a.tcl
+source Devices/Mesa/7i76.0/axis-b.tcl
