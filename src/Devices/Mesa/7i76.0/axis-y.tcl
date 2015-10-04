08c75c2b9d0d3a1f3dfadb8484a89b6626d60227
torvn77 <torvn77@mail.ru>
2015-10-04 03:01:32 +0300

    Update axis-y.tcl

--- src/Devices/Mesa/7i76.0/axis-y.tcl
+++ src/Devices/Mesa/7i76.0/axis-y.tcl
@@ -4,15 +4,15 @@
 
 # Step Gen signals/setup
 
-setp   hm2_5i25.0.stepgen.01.dirsetup        S::AXIS_1(DIRSETUP)
-setp   hm2_5i25.0.stepgen.01.dirhold         S::AXIS_1(DIRHOLD)
-setp   hm2_5i25.0.stepgen.01.steplen         S::AXIS_1(STEPLEN)
-setp   hm2_5i25.0.stepgen.01.stepspace       S::AXIS_1(STEPSPACE)
-setp   hm2_5i25.0.stepgen.01.position-scale  S::AXIS_1(STEP_SCALE)
+setp   hm2_5i25.0.stepgen.01.dirsetup        $::AXIS_1(DIRSETUP)
+setp   hm2_5i25.0.stepgen.01.dirhold         $::AXIS_1(DIRHOLD)
+setp   hm2_5i25.0.stepgen.01.steplen         $::AXIS_1(STEPLEN)
+setp   hm2_5i25.0.stepgen.01.stepspace       $::AXIS_1(STEPSPACE)
+setp   hm2_5i25.0.stepgen.01.position-scale  $::AXIS_1(STEP_SCALE)
 setp   hm2_5i25.0.stepgen.01.step_type        0
 setp   hm2_5i25.0.stepgen.01.control-type     0
-setp   hm2_5i25.0.stepgen.01.maxaccel         S::AXIS_1(STEPGEN_MAXACCEL)
-setp   hm2_5i25.0.stepgen.01.maxvel           S::AXIS_1(STEPGEN_MAXVEL)
+setp   hm2_5i25.0.stepgen.01.maxaccel         $::AXIS_1(STEPGEN_MAXACCEL)
+setp   hm2_5i25.0.stepgen.01.maxvel           $::AXIS_1(STEPGEN_MAXVEL)
 
 net y-pos-fb  <=  hm2_5i25.0.stepgen.01.position-fb
 net y-pos-cmd =>  hm2_5i25.0.stepgen.01.position-cmd
