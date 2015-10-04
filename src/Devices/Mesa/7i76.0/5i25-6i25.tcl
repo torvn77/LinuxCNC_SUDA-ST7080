07a88ef3bd18733e14702ad8f2392f6e32d62373
torvn77 <torvn77@mail.ru>
2015-10-04 03:43:37 +0300

    Update 5i25-6i25.tcl

--- src/Devices/Mesa/7i76.0/5i25-6i25.tcl
+++ src/Devices/Mesa/7i76.0/5i25-6i25.tcl
@@ -1,4 +1,7 @@
-loadrt hm2_pci config="num_encoders=1 num_pwmgens=0 num_3pwmgens=0 num_stepgens=5"
+
+loadrt $::HOSTMOT2(DRIVER) config=[lindex $::HOSTMOT2(CONFIG) 0]
+# board_ip=[lindex $::HOSTMOT2(IPADDR) 0]
+#loadrt hm2_pci config="num_encoders=1 num_pwmgens=0 num_3pwmgens=0 num_stepgens=5"
 #sserial_port_0=00xxxxxx
 #setp     hm2_5i25.0.watchdog.timeout_ns 10000000
 
