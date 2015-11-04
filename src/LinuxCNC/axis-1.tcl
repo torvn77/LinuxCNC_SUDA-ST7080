afa99a4d82e2c89a9e95bd6176da4ac9710dd199
torvn77 <torvn77@mail.ru>
2015-11-04 23:36:59 +0300

    Update axis-1.tcl

--- src/LinuxCNC/axis-1.tcl
+++ src/LinuxCNC/axis-1.tcl
@@ -7,5 +7,9 @@ net y-pos-cmd <= axis.1.motor-pos-cmd
 net y-pos-fb  => axis.1.motor-pos-fb  
 
 #  ---home signals---
+#net y-home-sw => axis.1.home-sw-in
 net min-home-y => axis.1.home-sw-in
-net min-home-y => axis.1.neg-lim-sw-in
+# ---limit switch signals---
+#net y-neg-limit => axis.1.neg-lim-sw-in
+net min-home-y  => axis.1.neg-lim-sw-in
+net y-pos-limit => axis.1.pos-lim-sw-in
