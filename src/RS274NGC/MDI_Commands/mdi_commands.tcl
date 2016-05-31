aaa34c105968a99afb48e4bef2b66f188f6467f3
torvn77 <linuxcnc@frezer>
2016-05-31 03:33:15 +0300

    comments

--- src/RS274NGC/MDI_Commands/mdi_commands.tcl
+++ src/RS274NGC/MDI_Commands/mdi_commands.tcl
@@ -1,8 +1,8 @@
-net manual.zero_x-axis                halui.mdi-command-00
-net manual.zero_y-axis                halui.mdi-command-01
-net manual.zero_z-axis                halui.mdi-command-02
-net manual.zero_a-axis                halui.mdi-command-03
-net manual.zero_b-axis                halui.mdi-command-04
-net manual.zeroed_all_axis             halui.mdi-command-05
-net manual.measumerent_material_height halui.mdi-command-06
-net manual.measumerent_tool_lenght     halui.mdi-command-07
+net panel.1.axis-x.change_g54_offset-0.0 halui.mdi-command-00
+net panel.1.axis-y.change_g54_offset-0.0 halui.mdi-command-01
+net panel.1.axis-z.change_g54_offset-0.0 halui.mdi-command-02
+net panel.1.axis-xyz.zeroed_g54_offset   halui.mdi-command-09
+
+net panel.1.measumerent_material_height  halui.mdi-command-60
+net panel.1.init_tool_lenght             halui.mdi-command-63
+#net panel.1.measumerent_tool_lenght      halui.mdi-command-64
