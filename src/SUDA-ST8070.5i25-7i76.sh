53cfe2efe4f30e857dec9e138fa4880993b9cb6f
torvn77 <torvn77@mail.ru>
2016-07-17 11:36:42 +0300

    Add LinuxCNC_SUDA-ST7080/src/SUDA-ST8070.5i25-7i76.sh script

---
+++ src/SUDA-ST8070.5i25-7i76.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+thisfile=$(readlink -f "$0")
+thisdir=$(dirname "$thisfile")
+cd "$thisdir"
+
+halrun -U
+
+#mate-terminal --geometry=150x65+800+28 --command 'linuxcnc SUDA-ST8070.5i25-7i76.ini'
+linuxcnc SUDA-ST8070.5i25-7i76.ini
+
+halrun -U
+read 
+exit
+
