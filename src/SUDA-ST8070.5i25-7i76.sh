e42e003c30a7bdacf812048e8c24f5cfad5d92e3
torvn77 <torvn77@mail.ru>
2017-03-20 00:26:02 +0300

    Correction ini and start script

--- src/SUDA-ST8070.5i25-7i76.sh
+++ src/SUDA-ST8070.5i25-7i76.sh
@@ -4,16 +4,30 @@ thisfile=$(readlink -f "$0")
 thisdir=$(dirname "$thisfile")
 cd "$thisdir"
 
-if [ -e SUDA-ST8070.5i25-7i76.ini.expanded ]; then rm SUDA-ST8070.5i25-7i76.ini.expanded; fi ;
+# 
 halrun -U
+if [ -e SUDA-ST8070.5i25-7i76.ini.expanded ]; then rm SUDA-ST8070.5i25-7i76.ini.expanded; fi ;
+
+disabled=0
+if [ "$disabled" -eq 1 ] ;
+	then echo -n;
+	elif [ ! -e "/tmp/LinuxCNC" ]; 
+		then
+			mkdir -p --mode=775 "/tmp/LinuxCNC";
+	elif [ -e "/tmp/LinuxCNC" ];
+		then
+			rm -fr "/tmp/LinuxCNC"
+			mkdir -p --mode=775 "/tmp/LinuxCNC";
+	else exit;
+fi;
 
 #mate-terminal --geometry=150x65+800+28 --command 'linuxcnc SUDA-ST8070.5i25-7i76.ini'
 linuxcnc SUDA-ST8070.5i25-7i76.ini
 
 halrun -U
 
-echo 'Pause !!!!!'
-read 
 
+#echo 'Pause !!!!!'
+#read 
 exit
 
